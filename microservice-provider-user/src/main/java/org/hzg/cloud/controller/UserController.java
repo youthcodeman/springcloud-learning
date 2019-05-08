6ab2bf728e026faf38aab74d97af94947411a89d
youthcodeman <hzgaly@163.com>
2019-05-08 23:41:11 +0800

    服务注册eureka注册中心

--- microservice-provider-user/src/main/java/org/hzg/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/org/hzg/cloud/controller/UserController.java
@@ -1,14 +1,24 @@
 package org.hzg.cloud.controller;
 
+import com.netflix.appinfo.InstanceInfo;
 import org.hzg.cloud.entity.User;
 import org.hzg.cloud.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 
 @RestController
 public class UserController {
+
+  @Autowired
+  private EurekaClient eurekaClient;
+
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
   @Autowired
   private UserRepository userRepository;
@@ -16,5 +26,17 @@ public class UserController {
   @GetMapping("/simple/{id}")
   public User findById(@PathVariable Long id) {
     return this.userRepository.findOne(id);
+  }
+
+  @GetMapping("/eureka-instance")
+  public String serviceUrl() {
+    InstanceInfo instance = this.eurekaClient.getNextServerFromEureka("MICROSERVICE-PROVIDER-USER", false);
+    return instance.getHomePageUrl();
+  }
+
+  @GetMapping("/instance-info")
+  public ServiceInstance showInfo() {
+    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+    return localServiceInstance;
   }
 }
