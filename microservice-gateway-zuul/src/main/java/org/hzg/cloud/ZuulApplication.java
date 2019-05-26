f65c740ddebfe647647cae79033a208f7a72bc05
youthcodeman <hzgaly@163.com>
2019-05-27 00:05:17 +0800

    zuul简单了解和基本配置

---
+++ microservice-gateway-zuul/src/main/java/org/hzg/cloud/ZuulApplication.java
@@ -0,0 +1,13 @@
+package org.hzg.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy  //因为zuul不包含任何的服务发现客户端，所以它需要依赖服务发现的组件来使用，@EnableZuulProxy是一个组合注解
+public class ZuulApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulApplication.class, args);
+  }
+}
