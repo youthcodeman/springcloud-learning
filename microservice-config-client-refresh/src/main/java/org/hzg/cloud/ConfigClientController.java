6ccdf0200b9b64f5ad75a0277959b94516292be2
youthcodeman <hzgaly@163.com>
2019-06-08 00:24:33 +0800

    spring cloud config 手动刷新配置

---
+++ microservice-config-client-refresh/src/main/java/org/hzg/cloud/ConfigClientController.java
@@ -0,0 +1,19 @@
+package org.hzg.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+  @Value("${document}")
+  private String profile;
+
+  @GetMapping("/profile")
+  public String getProfile() {
+    return this.profile;
+  }
+}
