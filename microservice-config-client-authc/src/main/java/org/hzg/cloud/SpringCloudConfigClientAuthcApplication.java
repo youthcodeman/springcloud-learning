ccfc381e48d5e7ede15721637714d6e192017203
youthcodeman <hzgaly@163.com>
2019-06-07 23:50:12 +0800

    springcloud config 权限设置 springcloud config 结合eureka使用

---
+++ microservice-config-client-authc/src/main/java/org/hzg/cloud/SpringCloudConfigClientAuthcApplication.java
@@ -0,0 +1,11 @@
+package org.hzg.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientAuthcApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringCloudConfigClientAuthcApplication.class, args);
+  }
+}
